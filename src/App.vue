6974563575d8afba88b2d9cc1f9c344436f19037
yangwan-cw <a2572213252@gmail.com>
2025-07-17 20:20:53 +0800

    fix: 文档显示异常
    README.MD 中存在乱码
    
    ISSUES CLOSED: #31

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,22 @@
 <template>
   <nav>
     <router-link to="/"
-    >Home
-    </router-link
-    >
+      >Home
+    </router-link>
     |
     <router-link to="/about"
-    >About
-    </router-link
-    >
+      >About
+    </router-link>
   </nav>
   <router-view />
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family:
+    Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
